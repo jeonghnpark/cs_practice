be94ba1988cb5d75b9f3fef9c5ffa36bd2b32e49
jhyeon <jeonghn.park@gmail.com>
2020-02-17 09:24:02 +0900

    first cs

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            var b = new A.B();
+            Console.WriteLine(b.GetValue());
+            Console.WriteLine(b.ToString());
+            var c = new C();
+            
+        }
+    }
+    public class A
+    {
+        private int value = 10;
+        public class B: A
+        {
+            public int GetValue()
+            {
+                return this.value;
+            }
+        }
+    }
+
+    public class C: A
+    {
+        public int GetValue()
+        {
+            return 0;
+        }
+    }
+}
